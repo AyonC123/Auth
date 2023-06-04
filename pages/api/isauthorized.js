a4175bf56c2608937e52eabd2061eb3503e22ac4
AyonC <ayonc@github.com>
2023-06-04 15:42:47 +0530

    change api

--- pages/api/isauthorized.js
+++ pages/api/isauthorized.js
@@ -4,8 +4,6 @@ import { authOptions } from "./auth/[...nextauth]";
 export default async function handler(req, res) {
 	const session = await getServerSession(req, res, authOptions);
 	if (session) {
-		// Signed in
-		console.log("Session", JSON.stringify(session, null, 2));
 		res.status(200).json({
 			isAuthorized: true,
 			data: session["user"],
