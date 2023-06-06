20a6cfc8dc45f596807d36369940ce7d89ec8729
AyonC <e10901ayon@dpsrkp.net>
2023-06-06 17:42:25 +0530

    finish python backend

--- py/views.py
+++ py/views.py
@@ -3,7 +3,6 @@ from flask import Blueprint, request, jsonify
 views = Blueprint(__name__, "views")
 
 @views.route("/signup", methods=["POST", "GET"])
-
 def signUp():
   if request.method == "POST":
     name = request.form["name"]
