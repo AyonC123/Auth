b5c15d44494fbcdd317d9312975d6067436e2601
AyonC <e10901ayon@dpsrkp.net>
2023-06-07 10:45:12 +0530

    finish python backend

--- py/views.py
+++ py/views.py
@@ -1,12 +1,23 @@
 from flask import Blueprint, request, jsonify
+import sqlite3
 
 views = Blueprint(__name__, "views")
 
+
 @views.route("/signup", methods=["POST", "GET"])
 def signUp():
-  if request.method == "POST":
-    name = request.form["name"]
-    email = request.form["email"]
-    return jsonify({"name": name, "email": email})
-  else:
-    return jsonify({})
+    con = sqlite3.connect("example.db")
+    cur = con.cursor()
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS signup (name text PRIMARY KEY, email text)"""
+    )
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        cur.execute(f"""INSERT OR IGNORE INTO signup VALUES ({name}, {email})""")
+        con.commit()
+        return jsonify({"name": name, "email": email})
+    else:
+        for row in con.execute("""SELECT * FROM signup"""):
+            print(row)
+        return jsonify({})
