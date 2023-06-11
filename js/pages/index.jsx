445f54aa0c362ed03db90403422852064d9c294b
AyonC <ayon@github.com>
2023-06-11 23:11:52 +0530

    make changes

--- js/pages/index.jsx
+++ js/pages/index.jsx
@@ -1,16 +1,16 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-	const { data: session } = useSession();
+  const { data: session } = useSession();
 
-	return (
-		<>
-			<h1>Hello World</h1>
-			{session ? (
-				<button onClick={() => signOut()}>Sign Out</button>
-			) : (
-				<button onClick={() => signIn()}>Sign In</button>
-			)}
-		</>
-	);
+  return (
+    <>
+      <h1>Hello World</h1>
+      {session ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
+    </>
+  );
 }
--- js/pages/user.jsx
+++ js/pages/user.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { useSession } from "next-auth/react";
 
 export default function User() {
-	const { data: session } = useSession({ required: true });
-	return (
-		<>
-			{session && (
-				<>
-					<h1>Name: {session.user.name}</h1>
-					<h1>Email: {session.user.email}</h1>
-					<img
-						src={session.user.image}
-						alt="profile image"
-						style={{ padding: "10px", borderRadius: "50px" }}
-					/>
-				</>
-			)}
-		</>
-	);
+  const { data: session } = useSession({ required: true });
+  return (
+    <>
+      {session && (
+        <>		
+          <h1>Name: {session.user.name}</h1>
+          <h1>Email: {session.user.email}</h1>
+          <img
+            src={session.user.image}
+            alt="profile image"
+            style={{ padding: "10px", borderRadius: "50px" }}
+          />
+        </>
+      )}
+    </>
+  );
 }
