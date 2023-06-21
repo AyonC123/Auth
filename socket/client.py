904ef3ab199df55db6c82f54a5de970261773989
AyonC <ayon@github.com>
2023-06-21 12:18:37 +0530

    somemore sockets

---
+++ socket/client.py
@@ -0,0 +1,14 @@
+import socket
+import json
+
+
+def test(args):
+    print(args)
+
+
+funcs = {"test": test}
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("127.0.0.1", 55555))
+    data = json.loads(s.recv(1024).decode("ascii"))
+    funcs[data["function"]](data["args"])
--- socket/main.py
+++ socket/main.py
@@ -1,5 +1,11 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 55555))
-server.listen()
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("127.0.0.1", 55555))
+    s.listen()
+    print("Server listening")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        conn.sendall(b"{\"function\": \"test\", \"args\": \"hello\"}")
