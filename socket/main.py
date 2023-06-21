5daa8b29e4d24c707019c223e15150dc07458a98
AyonC <ayon@github.com>
2023-06-21 12:34:02 +0530

    testing with socket

--- socket/main.py
+++ socket/main.py
@@ -5,7 +5,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("127.0.0.1", 55555))
     s.listen()
     print("Server listening")
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        conn.sendall(b"{\"function\": \"test\", \"args\": \"hello\"}")
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            conn.sendall(b"{\"function\": \"test\", \"args\": \"hello\"}")
+            while True:
+                print(conn.recv(1024).decode("ascii"))
