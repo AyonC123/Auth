3c109a59133b53003c5548fa56291248b7f67000
AyonC <ayon@github.com>
2023-07-11 23:05:58 +0530

    another try at parsing

--- htmlParser/testing.py
+++ htmlParser/testing.py
@@ -1,31 +1,38 @@
-string = "<p widht=\"100vw\" height=\"100vh\" color=\"black\">hello</p>"
+string = """
+<p widht=\"100vw\" height=\"100vh\" color=\"black\">hello</p>
+<p widht=\"100vw\" height=\"100vh\" color=\"black\">hello</p>
+<p        width="100 vh" height= "1760vw"    ></p>
+<p        width=100 height=1760    ></p>
+<span color="black"></span>
+"""
 
 i = 0
 while i < len(string):
     if string[i] == "<":
         i += 1
-        match string[i]:
-            case "p":
-                args = ""
-                value = ""
-                tag = ""
-                i += 1
-                while string[i] != ">":
-                    args += string[i]
-                    i += 1
-                i += 1
-                while string[i] != "<":
-                    value += string[i]
-                    i += 1
-                i += 1
-                if string[i] != "/":
-                    print("error")
-                i += 1
-                while string[i] != ">":
-                    tag += string[i]
-                    i += 1
-                if tag != "p":
-                    print("error")
-                print(f"tag: {tag}; args: {args}; value: {value};")
+        tag = ""
+        closingTag = ""
+        args = ""
+        value = ""
+        while string[i] != " ":
+            tag += string[i]
+            i += 1
+        while string[i] != ">":
+            args += string[i]
+            i += 1
+        i += 1
+        while string[i] != "<":
+            value += string[i]
+            i += 1
+        i += 1
+        if string[i] != "/":
+            print("error")
+        i += 1
+        while string[i] != ">":
+            closingTag += string[i]
+            i += 1
+        if tag != closingTag:
+            print("error")
+        print(f"tag: {tag}; args: {args.strip()}; value: {value};")
 
     i += 1
