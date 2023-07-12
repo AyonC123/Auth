001f0e33ed69b2ff23bbc433357542bca07effec
AyonC <ayon@github.com>
2023-07-12 23:12:25 +0530

    another try at parsing

--- htmlParser/testing.py
+++ htmlParser/testing.py
@@ -4,35 +4,68 @@ string = """
 <p        width="100 vh" height= "1760vw"    ></p>
 <p        width=100 height=1760    ></p>
 <span color="black"></span>
+<span>sdfasdf<p>dfd</p>dfadf<a></a></span>
 """
 
-i = 0
-while i < len(string):
-    if string[i] == "<":
-        i += 1
-        tag = ""
+
+def parse(index=0, oneOnly=False):
+    i = index
+    while i < len(string):
         closingTag = ""
+        tag = ""
         args = ""
         value = ""
-        while string[i] != " ":
-            tag += string[i]
+        if string[i] == "<":
             i += 1
-        while string[i] != ">":
-            args += string[i]
-            i += 1
-        i += 1
-        while string[i] != "<":
-            value += string[i]
-            i += 1
-        i += 1
-        if string[i] != "/":
-            print("error")
-        i += 1
-        while string[i] != ">":
-            closingTag += string[i]
-            i += 1
-        if tag != closingTag:
-            print("error")
-        print(f"tag: {tag}; args: {args.strip()}; value: {value};")
+            g = i
+            try:
+                while string[i] != " ":
+                    tag += string[i]
+                    i += 1
+                while string[i] != ">":
+                    args += string[i]
+                    i += 1
+            except:
+                i = g
+                tag = ""
+                args = ""
+                while string[i] != ">":
+                    tag += string[i]
+                    i += 1
 
-    i += 1
+            i += 1
+
+            def parseNested(i, string):
+                value = ""
+                while string[i] != "<":
+                    value += string[i]
+                    i += 1
+                i += 1
+                if string[i] != "/":
+                    i -= 1
+                    payload = parse(index=i, oneOnly=True)
+                    i = payload[0]  # type: ignore
+                    value += payload[1]  # type: ignore
+
+                    data = parseNested(i, string)
+                    value += data[0]
+                    i = data[1]
+                return [value, i]
+            data = parseNested(i, string)
+            value += data[0]
+            i = data[1]
+
+            i += 1
+            while string[i] != ">":
+                closingTag += string[i]
+                i += 1
+            if tag != closingTag:
+                print("error")
+            print(f"tag: {tag}; args: {args.strip()}; value: {value};")
+
+        i += 1
+        if oneOnly:
+            return [i, f"[tag: {tag}; args: {args.strip()}; value: {value};]"]
+
+
+parse()
