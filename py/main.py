bfeda3270453173cc5c9c84c066440975be9b56e
AyonC <ayon@github.com>
2024-01-14 19:54:41 +0530

    Format files

--- py/main.py
+++ py/main.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 from views import views
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix='/api')
+app.register_blueprint(views, url_prefix="/api")
 
-@app.route('/')
+
+@app.route("/")
 def home():
-  return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-  app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000)
